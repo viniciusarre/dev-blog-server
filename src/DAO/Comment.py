50bcc219767e7ce8c8b9db4a3c94951238592024
Vinícius <viniciusarre@gmail.com>
2020-09-23 08:01:59 -0300

    Add reference to next comment

--- src/DAO/Comment.py
+++ src/DAO/Comment.py
@@ -5,4 +5,5 @@ class Comment (Document):
     _id = ObjectIdField()
     user = StringField(required=True)
     comment = StringField(required=True)
-    is_thread = BooleanField(default=False)
+    is_thread = BooleanField(default=False),
+    next_comment = ObjectIdField()
